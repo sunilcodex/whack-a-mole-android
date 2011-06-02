01c5acdd1a554bf93fe3e00d9bc4f70494fc7829
lorenzo.lambertini@gmail.com <lorenzo.lambertini@gmail.com@7d1d8dcd-0236-fe96-d17c-a0558000c325>
2011-06-02 21:04:43 +0000

    -aggiunto lo score al gioco

--- trunk/SLT/src/slt/lmb/Slt.java
+++ trunk/SLT/src/slt/lmb/Slt.java
@@ -8,61 +8,67 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
 public class Slt extends Activity {
- 
-   	
+
+
 	private GraphicsMole im;
 	private int currentMolePos = -1;
-	
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        final GridView gw = (GridView) findViewById(R.id.gridview);       
-        im = new GraphicsMole(this);
-        gw.setAdapter(im);
-        
-        
-        gw.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-               // Toast.makeText(HelloAndroid.this, "position: " + position, Toast.LENGTH_SHORT).show();
-            	Log.d("cliccato", "colpito!!!!!!!");
-            	if (currentMolePos == position) {
-            		//mi hai cliccato
-            		
-            	}
+	private Score score;
+	private int scoreCurr = 0;
 
-            }
-        });
-        Handler step = new Step(); 
-        MoleGame mg = new MoleGame(step);
-        mg.start();
-        
-    }
-    private class Step extends Handler {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		score = new Score();
+		final GridView gw = (GridView) findViewById(R.id.gridview);       
+		im = new GraphicsMole(this);
+		gw.setAdapter(im);
 
-    	Mole mole=new Mole();
-    	Hole hole = new Hole();
-    	private int oldPosition=-1;
-    	
+
+		gw.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+
+				Log.d("cliccato", "colpito!!!!!!!");
+				if (currentMolePos == position) {
+					scoreCurr = score.AddScore();
+					Toast.makeText(Slt.this, "Score: " + scoreCurr,
+							Toast.LENGTH_SHORT).show();
+
+				}
+
+			}
+		});
+		Handler step = new Step(); 
+		MoleGame mg = new MoleGame(step);
+		mg.start();
+
+	}
+	private class Step extends Handler {
+
+		Mole mole=new Mole();
+		Hole hole = new Hole();
+		private int oldPosition=-1;
+
 		@Override
 		public void handleMessage(Message msg) {
-		Bundle bundle =msg.getData();
-		currentMolePos=bundle.getInt("newPosition");
-		
-		im.setItem(currentMolePos,mole.getTalpa());
-		
-		if(oldPosition!=-1 && currentMolePos != oldPosition) {
-			im.setItem(oldPosition,hole.getHole());	
+			Bundle bundle =msg.getData();
+			currentMolePos=bundle.getInt("newPosition");
+
+			im.setItem(currentMolePos,mole.getTalpa());
+
+			if(oldPosition!=-1 && currentMolePos != oldPosition) {
+				im.setItem(oldPosition,hole.getHole());	
+			}
+			oldPosition=currentMolePos;
+
+			im.notifyDataSetChanged();
+
 		}
-		oldPosition=currentMolePos;
-		
-		im.notifyDataSetChanged();
-		
-		}
-    	
-    	
-    }
+
+
+	}
 }
