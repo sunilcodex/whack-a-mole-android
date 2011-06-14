a4910fc5019f0db587fe61e3791baf00972e18e6
lorenzo.lambertini@gmail.com <lorenzo.lambertini@gmail.com@7d1d8dcd-0236-fe96-d17c-a0558000c325>
2011-06-14 06:25:01 +0000

    -aggiunta la grafica del menu

---
+++ trunk/SLT/src/slt/lmb/Messaggio.java
@@ -0,0 +1,17 @@
+package slt.lmb;
+
+import android.os.Handler;
+import android.os.Message;
+
+public abstract class Messaggio extends Handler {
+
+	@Override
+	public void handleMessage(Message msg) {
+		// TODO Auto-generated method stub
+		super.handleMessage(msg);
+	}
+	
+
+	
+	
+}
