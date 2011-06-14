670042cb2c34909fcf84c51897414d921079b71c
lorenzo.lambertini <lorenzo.lambertini@gmail.com>
2011-06-14 06:25:01 +0000

    -aggiunta la grafica del menu

---
+++ SLT/src/slt/lmb/Menu.java
@@ -0,0 +1,47 @@
+package slt.lmb;
+
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class Menu extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        
+        Button StartGameButton = (Button)findViewById(R.id.StartGame);
+        StartGameButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent StartGameIntent = new Intent(Menu.this,Slt.class);
+        		startActivity(StartGameIntent);
+        	}
+        });
+        
+        Button HelpButton = (Button)findViewById(R.id.Help);
+        HelpButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent HelpIntent = new Intent(Menu.this,Help.class);
+        		startActivity(HelpIntent);
+        	}
+        });
+      
+        
+        Button CreditsButton = (Button)findViewById(R.id.Credits);
+        CreditsButton.setOnClickListener(new OnClickListener() {
+        	
+        	public void onClick(View v) {
+        		Intent CreditsIntent= new Intent(Menu.this,Credits.class);
+        		startActivity(CreditsIntent);
+        	}
+        });
+    }
+}
