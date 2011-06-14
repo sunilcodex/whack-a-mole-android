4957f9540d80d9c75e790a8914140cbb33167f10
lorenzo.lambertini <lorenzo.lambertini@gmail.com>
2011-06-14 06:28:39 +0000

    -completamento progetto

---
+++ SLT/src/slt/lmb/Hole.java
@@ -0,0 +1,22 @@
+package slt.lmb;
+
+
+public class Hole {
+	
+	
+    Integer buca;
+	
+	public Hole(){
+		
+		buca=R.drawable.buca;
+	}
+
+	
+	
+	
+	public Integer getHole(){
+		
+		return buca;
+	}
+
+}
---
+++ SLT/src/slt/lmb/Mole.java
@@ -0,0 +1,23 @@
+package slt.lmb;
+
+
+
+
+public class Mole {
+	
+    Integer talpa;
+	
+	public Mole(){
+		
+		talpa=R.drawable.talpa;
+	}
+
+	
+	
+	
+	public Integer getTalpa(){
+		
+		return talpa;
+	}
+
+}
