ba4b5824db037e6b74cfd82d06a1683489dae8b1
lorenzo.lambertini@gmail.com <lorenzo.lambertini@gmail.com@7d1d8dcd-0236-fe96-d17c-a0558000c325>
2011-06-17 21:11:51 +0000

    aggiornato cambio immagine -punteggio e vite

--- trunk/SLT/src/slt/lmb/Slt.java
+++ trunk/SLT/src/slt/lmb/Slt.java
@@ -1,6 +1,7 @@
 package slt.lmb;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,64 +13,91 @@ import android.widget.AdapterView.OnItemClickListener;
 
 public class Slt extends Activity {
 
-
 	private GraphicsMole im;
 	private int currentMolePos = -1;
-	private Score score;
 	private int scoreCurr = 0;
-	
-	
+	private int lifeCurr = 3;
+	private Handler step = new ChangeImage();
+	private Handler Update = new Handler();
+	private MoleGame mg = new MoleGame(step);
+	private boolean isMole = true;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.startgame);
-		score = new Score();    
-	    final TextView tv=(TextView) findViewById(R.string.Score);	
+		final TextView scoreTview = (TextView) findViewById(R.id.Score);
+		final TextView lifeTview = (TextView) findViewById(R.id.Life);
 		final GridView gw = (GridView) findViewById(R.id.gridview);
 		im = new GraphicsMole(this);
 		gw.setAdapter(im);
-		/*
-		 * 
-		 * */
+	
+
+		// ----------------------------
+
 		gw.setOnItemClickListener(new OnItemClickListener() {
-			public void onItemClick(AdapterView<?> parent, View v,
+			public void onItemClick(AdapterView<?> parent, final View v,
 					int position, long id) {
 				if (currentMolePos == position) {
-					scoreCurr = score.AddScore();					
-					tv.setText("Score: " + scoreCurr);				
-					tv.refreshDrawableState();
+
+					Update.post(new Runnable() {
+
+						@Override
+						public void run() { 
+
+							if (isMole == true) {
+								scoreCurr = scoreCurr + 1;
+								scoreTview.setText("Score: " + scoreCurr);
+								scoreTview.refreshDrawableState();
+							} else {
+								lifeCurr = lifeCurr - 1;
+								lifeTview.setText("Life: " + lifeCurr);
+								lifeTview.refreshDrawableState();
+								if (lifeCurr == 0) {
+									mg.stopThread();
+									Intent gameOverIntent = new Intent(
+											Slt.this, GameOver.class);
+									startActivity(gameOverIntent);
+								}
+							}
+
+						}
+					});
 				}
 			}
 		});
-		Handler step = new Step();
-		MoleGame mg = new MoleGame(step);
+
 		mg.start();
+
 	}
-/*
- * 
- * 
- * */
-	private class Step extends Handler {
+
+	// ---------------------------
+	private class ChangeImage extends Handler {
 
 		Mole mole = new Mole();
 		Hole hole = new Hole();
 		private int oldPosition = -1;
-/*
- * 
- * 
- * */
+		private final double prob = 0.7;
+
 		@Override
 		public void handleMessage(Message msg) {
 			Bundle bundle = msg.getData();
 			currentMolePos = bundle.getInt("newPosition");
-			im.setItem(currentMolePos, mole.getTalpa());
-			
+			if (Math.random() < prob) {
+				im.setItem(currentMolePos, mole.getTalpa());
+				isMole = true;
+			} else {
+				im.setItem(currentMolePos, mole.getFarfalla());
+				isMole = false;
+			}
+
 			if (oldPosition != -1 && currentMolePos != oldPosition) {
 				im.setItem(oldPosition, hole.getHole());
 			}
-			
+
 			oldPosition = currentMolePos;
 			im.notifyDataSetChanged();
 		}
 	}
+
 }
